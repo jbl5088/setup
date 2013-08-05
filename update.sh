600110636d2b634eb5e5d95d3ac7564d3cdf939d
Nicholas Mattise <n.mattise@gmail.com>
2013-08-05 16:25:29 -0400

    added update

---
+++ update.sh
@@ -0,0 +1,25 @@
+#Stop the Existing server
+forever stop
+
+#Move the the htdocs root
+cd ~/
+
+#Remove the old platform
+sudo rm -r platform
+
+#git clone in the new one
+git clone https://github.com/eebhub/platform.git
+
+#move to platform
+cd platform
+
+#update the application if necessary
+sudo npm update
+
+#move back to the root
+cd ..
+
+#Start the app
+forever start app.js
+
+
