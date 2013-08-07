40ca454dd344cbb1d8eb7ac314f9c4024c763472
Jin Bin Li <jinbl19@gmail.com>
2013-08-07 13:46:28 -0400

    setup energyplus and openstudio

---
+++ rmt_setup.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+# download rmt_package from engr.psu.edu server
+wget -r --no-parent --reject "index.html*" 128.118.67.241/download/rmt_package
+
+
+# move rmt_package to the current directory
+mv 128.118.67.241/download/rmt_package/ ./
+sudo rm -r 128.118.67.241/
+
+
+# install energyplus and openstudio
+cd rmt_package/Eplus_OpenStudio
+sh installer.sh
